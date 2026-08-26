f5e79157f030aed69cb9b112759823b2eb088037
dsoma5749-commits <dsoma5749@gmail.com>
2026-08-26 19:27:18 +0530

    v120.0.0 - true zero dep 9.5KB

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
-
 setup(
     name="aquamarine-dredge",
-    version="115.0.0",
+    version="120.0.0",
     packages=find_packages(),
     install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'aquamarine-dredge=dredge.cli:main',
-        ],
-    },
+    entry_points={'console_scripts': ['aquamarine-dredge=dredge.cli:main']},
 )
